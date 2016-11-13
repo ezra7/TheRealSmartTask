dbe9e61b5b9da3835d1ff837ae2b533185215925
Ezra Aigbe <ezraaigbe7@gmail.com>
2016-11-12 19:40:41 -0800

    added an enter items

--- TaskApp2/app/src/main/java/com/ezraaigbe7gmail/taskapp/MainActivity.java
+++ TaskApp2/app/src/main/java/com/ezraaigbe7gmail/taskapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ezraaigbe7gmail.taskapp;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-}
+
+    }
+
+
