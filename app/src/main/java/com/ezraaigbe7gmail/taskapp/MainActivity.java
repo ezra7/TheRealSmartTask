27474ef94608c655053072e9a1f9516fc05f735b
Lexington Brill <lexingtonbrill@gmail.com>
2016-11-12 22:05:09 -0800

    Button functionality and allowed for text on TaskList or whatever

--- app/src/main/java/com/ezraaigbe7gmail/taskapp/MainActivity.java
+++ app/src/main/java/com/ezraaigbe7gmail/taskapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ezraaigbe7gmail.taskapp;
 
+import android.app.ListActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.nfc.Tag;
@@ -9,24 +10,34 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends ListActivity {
+    ArrayList<String> listItems=new ArrayList<String>();
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button button = (Button) findViewById(R.id.btnAddItem);
+
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,listItems);
+        setListAdapter(adapter);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 EditText box = (EditText)findViewById(R.id.etNewItem);
-
+                listItems.add(box.getText().toString());
+                adapter.notifyDataSetChanged();
             }
         });
+
+
     }
 }
 
--- app/src/main/java/com/ezraaigbe7gmail/taskapp/Tasks.java
+++ app/src/main/java/com/ezraaigbe7gmail/taskapp/Tasks.java
@@ -4,10 +4,7 @@ import android.widget.ArrayAdapter;
 
 public class Tasks{
     public Tasks(String description){
-        setContentView(R.layout.main);
-        adapter=new ArrayAdapter<Tasks>(this,
-                android.R.layout.simple_list_item_1, listItems);
-        setListAdapter(adapter);
+
     }
 
 
