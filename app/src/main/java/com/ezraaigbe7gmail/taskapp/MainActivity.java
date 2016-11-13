172a30458cd7e3ef40fefcdbcce48563758fc1de
Ezra Aigbe <ezraaigbe7@gmail.com>
2016-11-12 23:13:23 -0800

    updated layout

--- app/src/main/java/com/ezraaigbe7gmail/taskapp/MainActivity.java
+++ app/src/main/java/com/ezraaigbe7gmail/taskapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,12 +14,13 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends AppCompatActivity {
     ArrayList<String> listItems=new ArrayList<String>();
     ArrayAdapter<String> adapter;
     @Override
@@ -26,13 +28,14 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button button = (Button) findViewById(R.id.btnAddItem);
+        ListView view = (ListView)findViewById(R.id.lvItems);
 
         adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,listItems);
-        setListAdapter(adapter);
+        view.setAdapter(adapter);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 EditText box = (EditText)findViewById(R.id.etNewItem);
-                listItems.add(box.getText().toString());
+                listItems.add(/*0*/box.getText().toString());
                 adapter.notifyDataSetChanged();
             }
         });
