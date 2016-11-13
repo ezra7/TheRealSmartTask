cfa233e98779e971adf5902b126aec4707d0a4d4
Lexington Brill <lexingtonbrill@gmail.com>
2016-11-12 21:51:00 -0800

    new tasks

--- app/src/main/java/com/ezraaigbe7gmail/taskapp/Tasks.java
+++ app/src/main/java/com/ezraaigbe7gmail/taskapp/Tasks.java
@@ -1,8 +1,13 @@
 package com.ezraaigbe7gmail.taskapp;
 
+import android.widget.ArrayAdapter;
+
 public class Tasks{
     public Tasks(String description){
-
+        setContentView(R.layout.main);
+        adapter=new ArrayAdapter<Tasks>(this,
+                android.R.layout.simple_list_item_1, listItems);
+        setListAdapter(adapter);
     }
 
 
